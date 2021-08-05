199a4982536ecff55d746fc541712fe2488b36ed
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-08-05 21:00:03 +0300

    try doing hw

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ const timer = {
     setInterval(() => {
       const currentTime = Data.now();
       const deltaTime = currentTime - startTime;
+      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+      console.log(timeComponents);
     }, 1000);
   },
 };
 
 timer.start();
 
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+function getTimeComponents(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { days, hours, mins, secs };
+}

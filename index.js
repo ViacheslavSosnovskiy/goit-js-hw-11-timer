dca79467e4c3f9b696421d47b643646c3fa2a782
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-08-06 05:07:15 +0300

    +

--- index.js
+++ index.js
@@ -5,37 +5,43 @@ const refs = {
   secs: document.querySelector('[data-value="secs]'),
 };
 
-const timer = {
+class Timer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
   start() {
-    const startTime = Date.now();
-
     setInterval(() => {
       const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      const time = getTimeComponents(deltaTime);
-      updateClockface(time);
-      //   console.log(`${days}:${hours}:${mins}:${secs}`);
+      const deltaTime = this.targetDate - currentTime;
+      const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
+
+      refs.days.textContent = `${days}`;
+      refs.hours.textContent = `${hours}`;
+      refs.mins.textContent = `${mins}`;
+      refs.secs.textContent = `${secs}`;
     }, 1000);
-  },
-};
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+}
+
+const timer = new Timer({
+  selector: "#timer-1",
+  targetDate: new Date("Aug 15, 2021"),
+});
 
 timer.start();
-
-function updateClockface({ days, hours, mins, secs }) {
-  refs.clockface.textContant = `${days}:${hours}:${mins}:${secs}`;
-}
-
-function pad(value) {
-  return String(value).padStart(2, "0");
-}
-
-function getTimeComponents(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  return { days, hours, mins, secs };
-}

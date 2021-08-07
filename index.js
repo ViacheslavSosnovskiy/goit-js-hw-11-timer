2efa007aa42ff1d729f875e5e83f36607200fe64
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-08-07 14:27:59 +0300

    corrected a mistake

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const refs = {
   days: document.querySelector('[data-value="days"]'),
   hours: document.querySelector('[data-value="hours"]'),
   mins: document.querySelector('[data-value="mins"]'),
-  secs: document.querySelector('[data-value="secs]'),
+  secs: document.querySelector('[data-value="secs"]'),
 };
 
 class Timer {
